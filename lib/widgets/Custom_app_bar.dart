3c2b01754c70050da9b693f11e05769a3f9e4780
nautilus2100 <70881727+nautilus2100@users.noreply.github.com>
2023-12-23 00:54:07 -0600

    cambios1

--- lib/widgets/Custom_app_bar.dart
+++ lib/widgets/Custom_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:bibliotheka/page/NavBar.dart';
 import 'package:flutter/material.dart';
 import 'package:bibliotheka/core/color.dart';
 
@@ -7,39 +8,44 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: white,
+      color: black,
       margin: EdgeInsets.only(top: 50.0),
       height: 80.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //Icon(Icons.menu),
-          InkWell(
-            onTap: () {
-              print("boton verde de menu");
-            },
-            splashColor: white,
-            child: Container(
-              height: 65.0,
-              width: 50.0,
-              decoration: BoxDecoration(
-                color: green,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(10.0),
-                  bottomLeft: Radius.circular(20.0),
-                  bottomRight: Radius.circular(50.0),
-                ),
-              ),
-              child: Icon(
-                Icons.menu,
-                color: white,
-              ),
-            ),
-          )
-        ],
+      child: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(),
       ),
+
+      // child: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     //Icon(Icons.menu),
+      //     InkWell(
+      //       onTap: () {
+      //         print("boton verde de menu");
+      //       },
+      //       splashColor: white,
+      //       child: Container(
+      //         height: 65.0,
+      //         width: 50.0,
+      //         decoration: BoxDecoration(
+      //           color: green,
+      //           borderRadius: BorderRadius.only(
+      //             topLeft: Radius.circular(20.0),
+      //             topRight: Radius.circular(10.0),
+      //             bottomLeft: Radius.circular(20.0),
+      //             bottomRight: Radius.circular(50.0),
+      //           ),
+      //         ),
+      //         // child: Icon(
+      //         //   Icons.menu,
+      //         //   color: white,
+      //         // ),
+      //       ),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
